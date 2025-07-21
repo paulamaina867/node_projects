d3520a35cdf7d992d87ce76c49d40aa99d0e82ea
paulamaina867 <mainapauline867@gmail.com>
2025-07-21 18:44:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("my first node application")
+//import the chalk dpendency
+const chalk = require ("chalk")
+console.log(chalk.red("hello there"))
+
+
+
+//how to create a node application
+//how to install packages globally or locally
+//how to configure start script inside package.json file
+ //Create a new node project that contains a function which is find the sum of three numbers.
+// Inside, install chalk package an use it on the output
