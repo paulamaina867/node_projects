c5c365ce21d54e2ca4a29b946c502bc9e0fabce5
paulamaina867 <mainapauline867@gmail.com>
2025-07-24 22:08:52 +0300

    updates

--- callback/synchronus.js
+++
@@ -1,8 +0,0 @@
-//the program waits for one task before moving to the next
-const fs =require("fs");
-const chalk= require("chalk")
-
-data= fs.readFileSync('input.txt');//blocking
-console.log(chalk.bgBlue.whiteBright(data.toString()));
-console.log("program ended....")
-
